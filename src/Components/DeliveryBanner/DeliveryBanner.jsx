08a6d9cc1f486b153ad21bb4e88861df5b50ab54
Niyasperumannil <niyaskoottilakkadavu@gmail.com>
2025-09-09 12:19:45 +0530

    message

--- src/Components/DeliveryBanner/DeliveryBanner.jsx
+++ src/Components/DeliveryBanner/DeliveryBanner.jsx
@@ -1,57 +1,52 @@
-import React from 'react';
-import './DeliveryBanner.css';
+import React from "react";
+import "./DeliveryBanner.css";
 
 const DeliveryBanner = () => {
   return (
-    <div className="delivery-banner">
-      {/* Scheduled Delivery */}
-      <div className="scheduled-delivery">
-        <div className="icon-container">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="delivery-icon"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            strokeWidth="2"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M3 10h2l1 5h13l1-5h2M5 15v2a2 2 0 002 2h8a2 2 0 002-2v-2M16 7V3H8v4M6 7h12"
-            />
-          </svg>
+    <div className="delivery-banner-wrapper-outer">
+      <div className="delivery-banner-container">
+
+        {/* Left Section */}
+        <div className="delivery-left-section">
+          <div className="delivery-scheduled-box">
+            <div className="delivery-icon-circle">
+              <div className="icon-slice-bar" />
+            </div>
+            <span>Scheduled Delivery</span>
+          </div>
+
+          <div className="vertical-divider-line" />
+
+          <div className="free-delivery-label">
+            Free Delivery<br />
+            <strong>Over AED 350</strong>
+          </div>
         </div>
-        <span className="scheduled-text">Scheduled Delivery in Kerala</span>
-      </div>
 
-      {/* Free Delivery Info */}
-      <div className="free-delivery-info">
-        <span>Free Delivery</span>
-        <strong>Over ₹1000</strong>
-      </div>
+        {/* Center Logo Section */}
+        <div className="spinneys-logo-block">
+          <img
+            src="/WhatsApp_Image_2025-09-03_at_4.37.15_PM-removebg-preview.png"
+            alt="Spinneys Swift"
+            className="spinneys-logo-img"
+          />
+        </div>
 
-      <div className="divider"></div>
+        {/* Right Section */}
+        <div className="delivery-right-section">
+          <div className="delivery-info-box-left">
+            Delivery in<br />
+            <strong>60 minutes</strong>
+          </div>
 
-      {/* Logo */}
-      <div className="spinneys-logo-container">
-        <img
-          src="/WhatsApp_Image_2025-09-03_at_4.37.15_PM-removebg-preview.png"
-          alt="Kerala Swift"
-          className="spinneys-logo"
-        />
-      </div>
+          <div className="vertical-divider-line" />
 
-      {/* Delivery Time */}
-      <div className="delivery-time">
-        <strong>Delivery in</strong>
-        <span>3 Days</span>
-      </div>
+          <div className="delivery-info-box-right">
+            Minimum order<br />
+            <strong>AED 40</strong>
+          </div>
+        </div>
 
-      {/* Minimum Order */}
-      <div className="minimum-order">
-        <strong>Minimum order</strong>
-        <span>₹250</span>
       </div>
     </div>
   );
