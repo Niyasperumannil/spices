469481ad6c278e9bfa2fa9cde0eb9a6cdca50696
Niyasperumannil <niyaskoottilakkadavu@gmail.com>
2025-09-17 14:01:31 +0530

    message

--- src/Components/MakingProcess/MakingProcess.jsx
+++ src/Components/MakingProcess/MakingProcess.jsx
@@ -41,56 +41,59 @@ const MakingProcess = () => {
         <div className="process-icons">
           <div className="icon-item">
             <div className="icon-circle">
-              <FontAwesomeIcon icon={faLeaf} size="lg" color="#e93c3c" />
+              <FontAwesomeIcon icon={faLeaf} size="2x" color="#e93c3c" />
             </div>
             <span>Fresh &<br /> Ingredients <br /></span>
           </div>
 
           <div className="icon-item">
             <div className="icon-circle">
-              <FontAwesomeIcon icon={faDroplet} size="lg" color="#e93c3c" />
+              <FontAwesomeIcon icon={faDroplet} size="2x" color="#e93c3c" />
             </div>
             <span>Water</span>
           </div>
 
           <div className="icon-item">
             <div className="icon-circle">
-              <FontAwesomeIcon icon={faFlask} size="lg" color="#e93c3c" />
-            </div>
+  <FontAwesomeIcon icon={faFlask} size="2x" color="#e93c3c" />
+</div>
+
             <span>Preservation</span>
           </div>
 
           <div className="icon-item">
             <div className="icon-circle">
-              <FontAwesomeIcon icon={faBowlFood} size="lg" color="#e93c3c" />
+              <FontAwesomeIcon icon={faBowlFood} size="2x" color="#e93c3c" />
             </div>
             <span>Prepared</span>
           </div>
 
           <div className="icon-item">
             <div className="icon-circle">
-              <FontAwesomeIcon icon={faHeart} size="lg" color="#e93c3c" />
+              <FontAwesomeIcon icon={faHeart} size="2x" color="#e93c3c" />
             </div>
             <span>Healthy</span>
           </div>
 
           <div className="icon-item">
             <div className="icon-circle">
-              <FontAwesomeIcon icon={faPepperHot} size="lg" color="#e93c3c" />
+              <FontAwesomeIcon icon={faPepperHot} size="2x" color="#e93c3c" />
             </div>
-            <span>Spice <br />& Mix</span>
+<div class="icon-item">
+  <span class="one-line">Spices & Mix</span>
+</div>
           </div>
 
           <div className="icon-item">
             <div className="icon-circle">
-              <FontAwesomeIcon icon={faGlassWater} size="lg" color="#e93c3c" />
+              <FontAwesomeIcon icon={faGlassWater} size="2x" color="#e93c3c" />
             </div>
             <span>Traditional</span>
           </div>
 
           <div className="icon-item">
             <div className="icon-circle">
-              <FontAwesomeIcon icon={faStar} size="lg" color="#e93c3c" />
+              <FontAwesomeIcon icon={faStar} size="2x" color="#e93c3c" />
             </div>
             <span>Complex <br />& Flavor</span>
           </div>
